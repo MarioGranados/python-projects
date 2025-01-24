ce1000b082408b53f066a2a34fb961d75a677b5a
Mario Granados <mariogranados@Marios-MBP.attlocal.net>
2025-01-23 20:36:49 -0600

    main

--- GuessingGame_Mario_Granados.py
+++ GuessingGame_Mario_Granados.py
@@ -28,21 +28,23 @@ def start_game(random_number):
             print(f'You guessed the number {random_number}! It took you {count} tries.')
             #once the number is guessed we break loop
             break
-#game loop
-while True:
-    #prompt user if they want to play a game
-    print('Do you want to play the game?')
-    #ask user if for response and we uppercase it for simplicity
-    play_again = input('Type "yes" or "no": ').strip().upper()
-    #check if user wants to play
-    if play_again not in {'YES', 'NO'}:
-        print('You need to type "yes" or "no".')
-    elif play_again == 'NO':
-        print('Have a nice day!')
-        break
-    elif play_again == 'YES':
-        #generate random number between 1 - 100
-        random_number = random.randint(1, 100)
-        print('I’m thinking of a number between 1 and 100.')
-        #starts the game with new random number everytime
-        start_game(random_number)
+def main():
+    #game loop
+    while True:
+        #prompt user if they want to play a game
+        print('Do you want to play the game?')
+        #ask user if for response and we uppercase it for simplicity
+        play_again = input('Type "yes" or "no": ').strip().upper()
+        #check if user wants to play
+        if play_again not in {'YES', 'NO'}:
+            print('You need to type "yes" or "no".')
+        elif play_again == 'NO':
+            print('Have a nice day!')
+            break
+        elif play_again == 'YES':
+            #generate random number between 1 - 100
+            random_number = random.randint(1, 100)
+            print('I’m thinking of a number between 1 and 100.')
+            #starts the game with new random number everytime
+            start_game(random_number)
+main()
