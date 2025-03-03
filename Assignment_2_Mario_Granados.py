3c8670ca466f841b5dcbd5c1506cddd8d78b359e
Mario Granados <mariogranados@Marios-MBP.attlocal.net>
2025-03-03 12:03:03 -0600

    python project update

--- Assignment_2_Mario_Granados.py
+++ Assignment_2_Mario_Granados.py
@@ -1,35 +1,43 @@
 # Mario Granados
 # Assignment II
 
-#imports
+# imports
 import datetime
 
-#member classs with initialization of name, year of birth, weight(in lbs), and height(in inches)
+
+# member classs with initialization of name, year of birth, weight(in lbs), and height(in inches)
 class Member:
     def __init__(self, name, year_of_birth, weight_in_pounds, height_in_inches):
         self.name = name
-        self.year_of_birth = year_of_birth  # Store birth year, not age
+        self.year_of_birth = year_of_birth  
         self.weight_in_pounds = weight_in_pounds
         self.height_in_inches = height_in_inches
-    #getter for name
+
+    # getter for name
     def get_name(self):
         return self.name
-    #getter for age
+
+    # getter for age
     def get_age(self):
         return datetime.date.today().year - self.year_of_birth
+
     # update weight
     def set_weight(self, updated_weight):
         self.weight_in_pounds = updated_weight
+
     # updates weight to kilograms
     def weight_in_kilograms(self):
         return self.weight_in_pounds * 0.45359237
+
     # updates weight to meters
     def height_in_meters(self):
         return self.height_in_inches * 0.0254
+
     # bmi formula
     def calculate_bmi(self):
         return self.weight_in_kilograms() / (self.height_in_meters() ** 2)
-    # bmi interpretation from prev asignment 
+
+    # bmi interpretation from prev asignment
     def bmi_interpretation(self):
         bmi = self.calculate_bmi()
         if bmi < 18.5:
@@ -40,6 +48,7 @@ class Member:
             return "Overweight"
         else:
             return "Obese"
+
     # formatter
     def __str__(self):
         return f"The BMI for {self.get_name()} ({self.get_age()}) is {self.calculate_bmi():.2f} – {self.bmi_interpretation()}"
@@ -51,16 +60,17 @@ def main():
     # date = datetime.date(1997, 2, 19)
     # print(date.year)
     # print(datetime.date.today().year - date.year)
-    
+
     # members
     member1 = Member("John Doe", 1997, 180, 70)
     member2 = Member("Jane Smith", 1995, 150, 65)
-    #format print
+    # format print
     print(member1)
     print(member2)
     # test set weight method
     member1.set_weight(170)
     print("\nAfter weight change:")
     print(member1)
+
 
 main()
